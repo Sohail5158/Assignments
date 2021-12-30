ff0051b7ff68ee122e419ad8cc4e1ce256eeb7fe
Sohail5158 <94858933+Sohail5158@users.noreply.github.com>
2021-12-30 22:49:59 +0530

    Add files via upload

---
+++ dec 20/greaterthan10.js
@@ -0,0 +1,13 @@
+var num=10;
+if(num>10)
+{
+console.log("num is greater than 10");
+}
+else if(num<10)
+{
+console.log("num is not greater than 10");
+}
+else
+{
+console.log("num is equal to 10");
+}
---
+++ dec 20/maximum.js
@@ -0,0 +1,28 @@
+var n1=2;
+n1=parseInt(n1);
+var n2=6;
+n2=parseInt(n2);
+var n3=3;
+n3=parseInt(n3);
+if(n1>n2)
+{
+if(n1>n3)
+{
+console.log(n1);
+}
+else
+{
+console.log(n3);
+}
+}
+else
+{
+if(n2>n3)
+{
+console.log(n2);
+}
+else
+{
+console.log(n3);
+}
+}
