82f81c8dbab3dcd77811b29b50997ed63453b484
Sohail5158 <94858933+Sohail5158@users.noreply.github.com>
2021-12-30 22:59:35 +0530

    Add files via upload

---
+++ dec 21/minimum.js
@@ -0,0 +1,26 @@
+
+var n1=parseInt(prompt("enter 1st number"));
+var n2=parseInt(prompt("enter 2nd number"));
+var n3=parseInt(prompt("enter 3rd number"));
+if(n1<n2)
+{
+if(n1<n3)
+{
+console.log(n1);
+}
+else
+{
+console.log(n3);
+}
+}
+else
+{
+if(n2<n3)
+{
+console.log(n2);
+}
+else
+{
+console.log(n3);
+}
+}
---
+++ dec 21/vowel.js
@@ -0,0 +1,58 @@
+console.log("alert file loaded");
+var n=prompt("enter a alphabet");
+if(n=="a")
+{
+console.log("is a vowel");
+alert("vowel");
+}
+else if(n=="e")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="i")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="o")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="u")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="A")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="E")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="I")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="O")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="U")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+
+else
+ {
+alert("consonant");
+console.log("is a consonant")
+}
