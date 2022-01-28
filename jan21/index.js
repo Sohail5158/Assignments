f8662c48b4a62baf8f762db741fb7e87627837f8
mohammad sohail <94858933+Sohail5158@users.noreply.github.com>
2022-01-28 22:52:31 +0530

    Update index.js

--- jan21/index.js
+++ jan21/index.js
@@ -5,6 +5,9 @@ var img_arr = [
     'https://images.unsplash.com/photo-1574068468668-a05a11f871da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
     'https://images.unsplash.com/photo-1570527140771-020891229bb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
     'https://images.unsplash.com/photo-1612024782955-49fae79e42bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-hCE10SATmPW6IoZpUm6VJ2T5MwuPtNkhCA&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5QjPCzYAjpqeu8DflXcQg21SQmtxlPFzf5w&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDGHze6KftsR5ZM23KUBkUMOnQiNgguRWA&usqp=CAU',
 ];
 function imageClicked() {
     var img_prev_r = document.getElementById("img_preview");
